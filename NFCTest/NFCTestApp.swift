71cde41e7e4b6871f55284bc14839866bd422aa0
Jinsan Kim <gkjs1129@gmail.com>
2022-06-10 00:21:38 +0900

    Initial Commit

---
+++ NFCTest/NFCTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFCTestApp.swift
+//  NFCTest
+//
+//  Created by Jinsan Kim on 2022/06/10.
+//
+
+import SwiftUI
+
+@main
+struct NFCTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
